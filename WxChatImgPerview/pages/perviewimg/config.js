f1dfb449931a6fd64ad5b84c5db9c2e49624a8d5
herzshen <herzshen@tencent.com>
2021-07-29 20:32:39 +0800

    描述

--- WxChatImgPerview/pages/perviewimg/config.js
+++ WxChatImgPerview/pages/perviewimg/config.js
@@ -1,28 +1,74 @@
 export const IMG_ULRS = [
   {
     src: 'https://kf.qq.com/xv-test/fsna/kf-file/lx_pic/20210726/LXPIC_lxjihoa6g7_LXIMAGE_2016defa8ae541979ead8aee4a7575d5.jpg?s1=1627456888&s2=WB9VEbNtAYeUr5xdherzshen&s3=36000&s4=a13cb5c450163e7c5db752bc64608544e312af64',
-    matrix: '(1,0,0,1,0,0)'
+    matrix: '(1,0,0,1,0,0)',
+    origin: "0 0"
   },
   {
     src: 'https://kf.qq.com/xv-test/fsna/kf-file/lx_pic/20210726/LXPIC_lxjiunrthq_LXIMAGE_1df7f01ad448496084e00c3a61d91d4c.jpg?s1=1627456888&s2=smZnkNwkhcsQgpRGherzshen&s3=36000&s4=192e32e679bcb9aca7fcd998324c6c1acaa71eb7',
     matrix: '(1,0,0,1,0,0)',
+    origin: "0 0"
   },
   {
     src: 'https://kf.qq.com/xv-test/fsnb/kf-file/lx_pic/20210726/LXPIC_lx6pberiqr_LXIMAGE_78a1c0373f06472b9e0b19941967c748.jpg?s1=1627456888&s2=sEdXpmj0bd0Bm81Zherzshen&s3=36000&s4=8440193940d90bcef71afff6f00ecd1aa18a4abd',
     matrix: '(1,0,0,1,0,0)',
+    origin: "0 0"
   },
   {
     src: 'https://kf.qq.com/xv-test/fsna/kf-file/lx_pic/20210726/LXPIC_lx96tblhbv_LXIMAGE_76edc3ca23454a8a85c6df96e465f1f1.jpg?s1=1627456888&s2=WCatdUxSihjPydlsherzshen&s3=36000&s4=73eb8d4602dedb07bcc6d8727b1cadc5f066e1cd',
     matrix: '(1,0,0,1,0,0)',
+    origin: "0 0"
   },
   {
     src: 'https://kf.qq.com/xv-test/fsnb/kf-file/lx_pic/20210726/LXPIC_lxl36e3quo_LXIMAGE_83a665d71b3143cb8312aab66ca625fb.jpg?s1=1627456888&s2=q9me16dljbYaVdPTherzshen&s3=36000&s4=d2b0fb73e120baf0661d7237830dc4962d69a170',
     matrix: '(1,0,0,1,0,0)',
+    origin: "0 0"
   },
   {
     src: 'https://kf.qq.com/xv-test/fsna/kf-file/lx_pic/20210726/LXPIC_lxkis75kva_LXIMAGE_d32692e14ae34b67a70492bf84c17102.jpg?s1=1627456888&s2=ePB4VWVShwhIvUEqherzshen&s3=36000&s4=cdce2343e16475d393cef363087553918e84d6c0',
     matrix: '(1,0,0,1,0,0)',
+    origin: "0 0"
   }
   
-  
 ]
+
+class ClickEvent{
+  constructor(){
+    // 触摸开始时间
+    this.touchStartTime = 0
+    // 触摸结束时间
+    this.touchEndTime = 0  
+    // 最后一次单击事件点击发生时间
+    this.lastTapTime = 0 
+    // 单击事件点击后要触发的函数
+    this.lastTapTimeoutFunc = null 
+  }
+  bindEvent(e, fn) {
+    // 控制点击事件在350ms内触发，加这层判断是为了防止长按时会触发点击事件
+    if (this.touchEndTime - this.touchStartTime < 350) {
+      // 当前点击的时间
+      var currentTime = e.timeStamp
+      var lastTapTime = this.lastTapTime
+      // 更新最后一次点击时间
+      this.lastTapTime = currentTime
+      // 如果两次点击时间在300毫秒内，则认为是双击事件
+      if (currentTime - lastTapTime < 300) {
+        // console.log("double tap")
+        // 成功触发双击事件时，取消单击事件的执行
+        clearTimeout(this.lastTapTimeoutFunc);
+        fn('double_tap', e)
+      } else {
+        // 单击事件延时300毫秒执行，这和最初的浏览器的点击300ms延时有点像。
+        this.lastTapTimeoutFunc = setTimeout(function () {
+          console.log("tap")
+          fn('tap', e)
+        }, 300);
+      }
+    }
+  }
+
+}
+
+export {
+  ClickEvent,
+}
--- WxChatImgPerview/pages/perviewimg/index.js
+++ WxChatImgPerview/pages/perviewimg/index.js
@@ -1,12 +1,13 @@
 // pages/perviewimg/index.js
-import { IMG_ULRS } from './config'
+import { IMG_ULRS, ClickEvent } from './config'
+const EventObj = new ClickEvent();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    matrix: '',
+  
     height: 500,
     width: 375,
     imgH: 240,
@@ -14,8 +15,23 @@ Page({
     contentH: '100%',
     contentW: '100%',
     isScale: false,
-    scaleNum: 4,
+    scaleNum: 3,
+    // perviewTop: 0,
+    perviewLeft: 0,
+    marginTop: 0,
+   
+    scrollLeft: 0,
+    scaleIndex: -1,
+
     imgs: IMG_ULRS,
+    timeer: null,
+    scrollIng: false,
+    startScorllLeft: 0,
+    endScorllLeft: 0,
+    isScorll: true,
+
+    distance: 0,//手指移动的距离
+    
   },
 
   /**
@@ -24,8 +40,8 @@ Page({
   onLoad: function (options) {
     try {
       const res = wx.getSystemInfoSync()
-      console.log(res.windowWidth - 240)
-      console.log(res.windowHeight - 320)
+      // console.log(res.windowWidth - 240)
+      // console.log(res.windowHeight - 320)
       this.setData({
         height: res.windowHeight,
         width: res.windowWidth,
@@ -34,51 +50,163 @@ Page({
       // Do something when catch error
     }
   },
-  bindScroll(event){
-    console.log(event.detail )
-  },
-  bindDragStart(event){
-    console.log('bindDragStart',event.detail )
-    this.data.startLeft = event.detail.scrollLeft 
-  },
-  bindDragEnd(event){
-    console.log('bindDragEnd',event.detail )
-    this.data.endLeft = event.detail.scrollLeft 
-    let { startLeft, scrollLeft, width } = this.data
-    if(startLeft < 0) return
-    // if (startLeft > this.data.endLeft && startLeft - this.data.endLeft >= width/2 ) {
-    //   // 左
-    //   scrollLeft -= width
-    //   console.log('scrollLeft 左', scrollLeft)
-    // } else if (startLeft < this.data.endLeft  && this.data.endLeft - startLeft >= width/2) {
-    //   // 右
-    //   scrollLeft += width
-    //   console.log('scrollLeft 右', scrollLeft)
-    // } 
-  
-    // this.setData({
-    //   scrollLeft,
-    // });
-  },
-  onScale(){
 
-  },
-  bindTap(e){
-    let {scaleNum, imgH, imgW, contentH, contentW, height, width} = this.data;
+  handleDayClick(e){
+    // let {offsetLeft,offsetTop } = e.currentTarget
+    let {x} = e.detail;
+    let { index, item } = e.currentTarget.dataset
+    let {imgs, scaleNum,imgH, imgW, width, height, contentH, contentW, isScale, marginTop} = this.data
+    let imgItem = `imgs[${index}]`
     let tMatrix = Array(6).fill(0)
-    tMatrix[0] = scaleNum;
-    tMatrix[3] = scaleNum;
-    var temp = tMatrix.join(",");
-    this.setData({
-      isScale: true,
-      matrix: temp,
-      contentH: imgH * scaleNum +  width - imgW,
-      contentW: imgW * scaleNum +  width - imgW,
-    });
-    console.log('bindTap', temp)
-  }
+    EventObj.bindEvent(e, (type, event)=>{
+      // console.log(type, e)
+      tMatrix[0] = scaleNum;
+      tMatrix[3] = scaleNum;
+      if (type === 'double_tap') {
+        if (item.isScale) {
+          this._initScale(item, index, imgs)
+          return
+        }
+        this.data.scaleIndex = index
+        imgs[index].matrix = tMatrix
+        imgs[index].isScale = true
+        contentH =  imgH * scaleNum
+        contentW =  imgW * scaleNum
+        if (contentH > height) {
+          marginTop = (contentH - height) / 2 + 'Px';
+          imgs[index].marginTop = marginTop
+        } 
+        this.setData({
+          contentH,
+          contentW,
+          marginTop,
+          [imgItem]: imgs[index],
+          isScorll: false,
+       
+          // scaleIndex: index,
+        },()=>{
+          // setTimeout(() => {
+          //   this.setData({
+          //     isScorll: false,
+          //   })
+          // }, 200);
+          
+        })
+      } else if (type === 'tap') {
+        this._initScale(item, index, imgs)
+      }
 
+    })
+  },
+  _initScale(item, index, imgs){
+    let imgItem = `imgs[${index}]`
+    let tMatrix = Array(6).fill(0)
+    if (item.isScale) {
+      tMatrix[0] = 1;
+      tMatrix[3] = 1;
+      imgs[index].matrix = tMatrix
+      imgs[index].isScale = false
+      imgs[index].marginTop = 0
+      this.data.scaleIndex = -1
+      this.setData({
+        isScorll: true,
+        // scaleIndex: -1,
+        [imgItem]: imgs[index],
+      })
+    }
 
+  },
+  bindScrollViewToupper(e){
 
   
+  },
+  bindScrollViewTolower(e){
+  
+  },
+
+  bindScroll(e){
+
+    if ( this.data.timeer) clearTimeout( this.data.timeer)
+    this.data.timeer = setTimeout(() => {
+      let { width, scaleIndex, imgs} = this.data
+   
+      wx.createSelectorQuery()
+        .select('.scroll-img-main')
+        .scrollOffset()
+        .exec((res) => {
+         
+          // console.log('bindTouchEnd===',res)
+          let scrollView = res[0];
+          // console.log('bindTouchEnd===', scrollView.scrollLeft)
+          console.log('_initScale====', parseInt(scrollView.scrollLeft/width), scaleIndex)
+          if ( parseInt(scrollView.scrollLeft/width) !== scaleIndex && scaleIndex !== -1) {
+            this.setData({
+              isScorll: false,
+            });
+            this._initScale(imgs[scaleIndex], scaleIndex, imgs)
+          }
+        })
+    }, 1000);
+  },
+  bindTouchEnd(){
+   
+  },
+  /**
+   * 双手指触发开始 计算开始触发两个手指坐标的距离
+   */
+  touchstartCallback: function(e) {
+    // 单手指缩放开始，不做任何处理
+    if (e.touches.length == 1) return;
+    // 当两根手指放上去的时候，将距离(distance)初始化。
+    let xMove = e.touches[1].clientX - e.touches[0].clientX;
+    let yMove = e.touches[1].clientY - e.touches[0].clientY;
+    //计算开始触发两个手指坐标的距离
+    let distance = Math.sqrt(xMove * xMove + yMove * yMove);
+    this.setData({
+      'distance': distance,
+    })
+  },
+
+  /**
+   * 双手指移动   计算两个手指坐标和距离
+   */
+  touchmoveCallback: function(e) {
+    return
+    // 单手指缩放不做任何操作
+    if (e.touches.length == 1) return;
+    //双手指运动 x移动后的坐标和y移动后的坐标
+    let xMove = e.touches[1].clientX - e.touches[0].clientX;
+    let yMove = e.touches[1].clientY - e.touches[0].clientY;
+    //双手指运动新的 ditance
+    let distance = Math.sqrt(xMove * xMove + yMove * yMove);
+    //计算移动的过程中实际移动了多少的距离
+    let distanceDiff = distance - this.data.distance;
+    let newScale = this.data.scaleNum + 0.005 * distanceDiff
+    newScale = newScale < 1 ?  1 : newScale
+    console.log('newScale', newScale)
+    // 为了防止缩放得太大，所以scale需要限制，同理最小值也是
+  
+    let { index } = e.currentTarget.dataset
+    let {imgs, imgH, imgW, height, contentH, contentW, marginTop} = this.data
+    let imgItem = `imgs[${index}]`
+    let tMatrix = Array(6).fill(0)
+    tMatrix[0] = newScale;
+    tMatrix[3] = newScale;
+    // console.log(type, e)
+    this.data.scaleIndex = index
+    imgs[index].matrix = tMatrix
+    imgs[index].isScale = true
+    contentH =  imgH * newScale
+    contentW =  imgW * newScale
+    this.setData({
+      contentH,
+      contentW,
+      marginTop,
+      [imgItem]: imgs[index],
+      isScorll: false,
+      // scaleIndex: index,
+    })
+  },
+  
+
 })
