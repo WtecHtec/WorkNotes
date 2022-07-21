a6866bf2bb15e237dedc502adadbec9da731bb7c
herzshen <herzshen@tencent.com>
2022-07-21 22:24:30 +0800

    修复属性大小问题

--- WebPageNotes/codeSign/js/src/atom/atom.js
+++ WebPageNotes/codeSign/js/src/atom/atom.js
@@ -4,9 +4,10 @@
  * @param {*} value
  * @returns
  */
- function getAtomCss(atomMap, prop, value) {
-  if (!atomMap[prop]) return null;
-  return atomMap[prop][value] || null;
+function getAtomCss(atomMap, prop, value) {
+	if (!atomMap[prop]) return null;
+	value = (PropFormat[prop] ? PropFormat[prop](value) : value);
+	return atomMap[prop][value] || null;
 }
 
 /**
@@ -15,53 +16,53 @@
  * @param {*} isPseudo 是否开启伪元素替换
  * @returns
  */
- function getGatherAtomCss(gatherConfig, nodes, isPseudo, pseudoType) {
-  const atomCss = [];
-  let newNodes = [];
-  gatherConfig.forEach(({ name, props, pseudo }) => {
-    // 当不执行伪元素类名时，排除伪元素原子配置
-    if (!isPseudo && pseudo) {
-      return;
-    }
-    // 当执行伪元素类名时，排除不是伪元素原子配置
-    if (isPseudo && !pseudo) {
-      return;
-    }
-    // 当执行伪元素类名时，排除伪元素类型不一致的原子配置
-    if (isPseudo && pseudoType !== pseudo) {
-      return;
-    }
+function getGatherAtomCss(gatherConfig, nodes, isPseudo, pseudoType) {
+	const atomCss = [];
+	let newNodes = [];
+	gatherConfig.forEach(({ name, props, pseudo }) => {
+		// 当不执行伪元素类名时，排除伪元素原子配置
+		if (!isPseudo && pseudo) {
+			return;
+		}
+		// 当执行伪元素类名时，排除不是伪元素原子配置
+		if (isPseudo && !pseudo) {
+			return;
+		}
+		// 当执行伪元素类名时，排除伪元素类型不一致的原子配置
+		if (isPseudo && pseudoType !== pseudo) {
+			return;
+		}
 
-    let num = 0;
-    newNodes = [];
-    // 遍历样式ast属性
-    nodes.forEach((node) => {
-      const { prop, value, important } = node;
-      if (important) {
-        newNodes.push(node);
-      } else {
-        // 查找属性值匹配
-        const fItem = props.find(
-          item => prop === item.prop && (PropFormat[prop] ? PropFormat[prop](value) : value) === item.value,
-        );
-        if (fItem) {
-          num += 1;
-        } else {
-          newNodes.push(node);
-        }
-      }
-    });
-    if (num === props.length) {
-      atomCss.push(name);
-      nodes = newNodes;
-    }
-  });
-  return [atomCss, nodes];
+		let num = 0;
+		newNodes = [];
+		// 遍历样式ast属性
+		nodes.forEach((node) => {
+			const { prop, value, important } = node;
+			if (important) {
+				newNodes.push(node);
+			} else {
+				// 查找属性值匹配
+				const fItem = props.find(
+					item => prop === item.prop && (PropFormat[prop] ? PropFormat[prop](value) : value) === item.value,
+				);
+				if (fItem) {
+					num += 1;
+				} else {
+					newNodes.push(node);
+				}
+			}
+		});
+		if (num === props.length) {
+			atomCss.push(name);
+			nodes = newNodes;
+		}
+	});
+	return [atomCss, nodes];
 }
 
 const simplifyAttr = {
-  margin: ['unset', 'auto', '0 auto', '0'],
-  padding: ['unset', '0'],
+	margin: ['unset', 'auto', '0 auto', '0'],
+	padding: ['unset', '0'],
 };
 /**
  * 拆解简写的 margin/padding 边距值为四个方向的边距值
@@ -70,32 +71,32 @@ const simplifyAttr = {
  * @returns
  */
 function dealMarginPaddingValue(prop, value) {
-  let returnVal = '';
-  // 判断属性名是否为 margin/padding,排除特定属性值(存在原子属性): padding为 unset 或 0; margin为 unset 或 auto 或 0 auto 或 0
-  if (simplifyAttr[prop] && !simplifyAttr[prop].includes(value)) {
-    const reg = /0|-\d+rpx|\d+rpx|\d+.\d+rpx/g; // 匹配简写的边距值
-    if (new RegExp(reg).test(value)) { // 校验通过
-      const valueArr = value.split(' ');
-      if (!valueArr.length) return;
-      let ruleVal;
-      if (valueArr.length == 1) {
-        ruleVal = [0, 0, 0, 0];
-      } else if (valueArr.length == 2) {
-        ruleVal = [0, 1, 0, 1];
-      } else if (valueArr.length == 3) {
-        ruleVal = [0, 1, 2, 1];
-      } else {
-        ruleVal = [0, 1, 2, 3];
-      }
-      returnVal = [
-        { prop: `${prop}-top`, value: valueArr[ruleVal[0]] },
-        { prop: `${prop}-right`, value: valueArr[ruleVal[1]] },
-        { prop: `${prop}-bottom`, value: valueArr[ruleVal[2]] },
-        { prop: `${prop}-left`, value: valueArr[ruleVal[3]] },
-      ];
-    }
-  }
-  return returnVal;
+	let returnVal = '';
+	// 判断属性名是否为 margin/padding,排除特定属性值(存在原子属性): padding为 unset 或 0; margin为 unset 或 auto 或 0 auto 或 0
+	if (simplifyAttr[prop] && !simplifyAttr[prop].includes(value)) {
+		const reg = /0|-\d+rpx|\d+rpx|\d+.\d+rpx/g; // 匹配简写的边距值
+		if (new RegExp(reg).test(value)) { // 校验通过
+			const valueArr = value.split(' ');
+			if (!valueArr.length) return;
+			let ruleVal;
+			if (valueArr.length == 1) {
+				ruleVal = [0, 0, 0, 0];
+			} else if (valueArr.length == 2) {
+				ruleVal = [0, 1, 0, 1];
+			} else if (valueArr.length == 3) {
+				ruleVal = [0, 1, 2, 1];
+			} else {
+				ruleVal = [0, 1, 2, 3];
+			}
+			returnVal = [
+				{ prop: `${prop}-top`, value: valueArr[ruleVal[0]] },
+				{ prop: `${prop}-right`, value: valueArr[ruleVal[1]] },
+				{ prop: `${prop}-bottom`, value: valueArr[ruleVal[2]] },
+				{ prop: `${prop}-left`, value: valueArr[ruleVal[3]] },
+			];
+		}
+	}
+	return returnVal;
 }
 
 /**
@@ -105,58 +106,58 @@ function dealMarginPaddingValue(prop, value) {
  * @returns 
  */
 function getCssNodesByStyle(styleContent) {
-  const result = [];
-  if (styleContent && styleContent[0] === '{' && styleContent[styleContent.length - 1] === '}') {
-    const datas = styleContent.substring(1, styleContent.length - 2).split(';')
-    datas.forEach(item => {
-      if (item) {
-        const nodes = item.split(':');
-        if (nodes.length === 2 && nodes[0] && nodes[1]) {
-          // 处理mixin的情况
-          if (STYLE_MIXIN[nodes[0]] && typeof STYLE_MIXIN[nodes[0]] === 'function') {
-            const cssStr =  STYLE_MIXIN[nodes[0]](...nodes[1].split(','));
-            if (cssStr) {
-              const mixinDatas = cssStr.split(';')
-              mixinDatas.forEach(mItem => {
-                const minxinNodes = mItem.split(':');
-                if (minxinNodes.length === 2 && minxinNodes[0] && minxinNodes[1]) { 
-                  result.push({
-                    prop: minxinNodes[0].trim(),
-                    value: htmlDecodeByRegExp(minxinNodes[1].trim()),
-                  })
-                }
-              })
-            }
-          } else {
-            result.push({
-              prop: nodes[0].trim(),
-              value: htmlDecodeByRegExp(nodes[1].trim()) ,
-            })
-          }
-        }
-      }
-    })
-  }
-  return result;
+	const result = [];
+	if (styleContent && styleContent[0] === '{' && styleContent[styleContent.length - 1] === '}') {
+		const datas = styleContent.substring(1, styleContent.length - 2).split(';')
+		datas.forEach(item => {
+			if (item) {
+				const nodes = item.split(':');
+				if (nodes.length === 2 && nodes[0] && nodes[1]) {
+					// 处理mixin的情况
+					if (STYLE_MIXIN[nodes[0]] && typeof STYLE_MIXIN[nodes[0]] === 'function') {
+						const cssStr = STYLE_MIXIN[nodes[0]](...nodes[1].split(','));
+						if (cssStr) {
+							const mixinDatas = cssStr.split(';')
+							mixinDatas.forEach(mItem => {
+								const minxinNodes = mItem.split(':');
+								if (minxinNodes.length === 2 && minxinNodes[0] && minxinNodes[1]) {
+									result.push({
+										prop: minxinNodes[0].trim(),
+										value: htmlDecodeByRegExp(minxinNodes[1].trim()),
+									})
+								}
+							})
+						}
+					} else {
+						result.push({
+							prop: nodes[0].trim(),
+							value: htmlDecodeByRegExp(nodes[1].trim()),
+						})
+					}
+				}
+			}
+		})
+	}
+	return result;
 }
 
 
 
 function formatStyles(datas) {
-  const nodeMap = {};
-  datas.forEach(item => {
-    nodeMap[item.prop] = item;
-  })
-  const nodes = Object.values(nodeMap);
-  let result = [];
-  nodes.forEach(item => {
-    const { prop , value } = item;
-    if (['margin', 'padding'].indexOf(prop) !== -1) {
-      const dealMPs = dealMarginPaddingValue(prop, value);
-      result = result.concat(dealMPs);
-    } else {
-      result.push(item);
-    }
-  })
-  return result;
+	const nodeMap = {};
+	datas.forEach(item => {
+		nodeMap[item.prop] = item;
+	})
+	const nodes = Object.values(nodeMap);
+	let result = [];
+	nodes.forEach(item => {
+		const { prop, value } = item;
+		if (['margin', 'padding'].indexOf(prop) !== -1) {
+			const dealMPs = dealMarginPaddingValue(prop, value);
+			result = result.concat(dealMPs);
+		} else {
+			result.push(item);
+		}
+	})
+	return result;
 }
--- WebPageNotes/codeSign/js/src/top-opt/view.js
+++ WebPageNotes/codeSign/js/src/top-opt/view.js
@@ -2,25 +2,25 @@
 const be_config = { indent_size: 2, space_in_empty_paren: true }
 function createView() {
 	if (g_form_data_map && Object.values(g_form_data_map).length) {
-    $('#hz_loading').show();
+		$('#hz_loading').show();
 		const formDatas = Object.values(g_form_data_map).sort((a, b) => a.rankNum - b.rankNum);
 		const tree = formatArrayToTree(formDatas);
 		const parentDom = createWxView();
-    const cssData = {};
+		const cssData = {};
 		createDom(tree, parentDom, cssData);
 		$('.wtc_diaglog').show();
 		setTimeout(() => {
-			const html = htmlDecodeByRegExp( parentDom.html());
-      console.log(html_beautify(html , be_config), html)
-			$('#html_code').text(html_beautify(html , be_config));
-      if (Object.keys(cssData).length) {
-        const css = Object.values(cssData);
-        css.length && $('#css_code').text(css_beautify(css.join(''), be_config));
-      }
-      hljs.highlightAll()
-      setTimeout(()=> {
-        $('#hz_loading').hide();
-      }, 200);
+			const html = htmlDecodeByRegExp(parentDom.html());
+			console.log(html_beautify(html, be_config), html)
+			$('#html_code').text(html_beautify(html, be_config));
+			if (Object.keys(cssData).length) {
+				const css = Object.values(cssData);
+				css.length && $('#css_code').text(css_beautify(css.join(''), be_config));
+			}
+			hljs.highlightAll()
+			setTimeout(() => {
+				$('#hz_loading').hide();
+			}, 200);
 		}, 0)
 	}
 }
@@ -29,52 +29,52 @@ function createDom(data, parentDom, cssData) {
 	if (Array.isArray(data) && data.length) {
 		data.forEach(item => {
 			const dom = createWxView();
-      let atomCss = [];
-      let otherNodes = [];
-      if (item.style) {
-        const styleDatas = getCssNodesByStyle(item.style.replace(/\r|\n|\\s/g, ''));
-        const nodes = formatStyles(styleDatas);
-        // 先转原子类
-        [ atomCss = [], otherNodes = []]=  getGatherAtomCss(g_atom_gather, nodes);
-        const remainNodes  = [];
-        otherNodes.forEach(item => {
-          const { prop, value } = item;
-          const atom = getAtomCss(g_atom_base, prop, value);
-          if (atom) {
-            atomCss.push(atom);
-            return
-          }
-          remainNodes.push(`${prop}:${value};`);
-        })
-        if (item.className && remainNodes && remainNodes.length ) {
-          atomCss.push(item.className);
-          cssData[`.${item.className}`] = `.${item.className}{${remainNodes.join(' ')}}`;
-        } else if (remainNodes && remainNodes.length) {
-          dom.attr('style', remainNodes.join());
-        }
-      }
-      // 伪类
-      if (item.afterContent && item.className) {
-        const styleDatas = getCssNodesByStyle(item.afterContent.replace(/\r|\n|\\s/g, ''));
-        const nodes = formatStyles(styleDatas);
-        const [ gatherAtomCss = [], gatherOtherNodes = []]=  getGatherAtomCss(g_atom_gather, nodes, 1, 'after');
-        if (gatherAtomCss.length) {
-          atomCss = atomCss.concat(gatherAtomCss);
-        }
-        if (gatherOtherNodes.length) {
-          let afterStr = '';
-          gatherOtherNodes.forEach(gItem => {
-            afterStr = `${afterStr}${gItem.prop}:${gItem.value};`
-          })
-          cssData[`.${item.className}::after`] = `.${item.className}::after{${afterStr}}`;
-        }
-        if (!atomCss.includes(item.className)) {
-          atomCss.push(item.className);
-        }
-      }
-      if (atomCss.length) {
-        dom.attr('class', atomCss.join(' '));
-      }
+			let atomCss = [];
+			let otherNodes = [];
+			if (item.style) {
+				const styleDatas = getCssNodesByStyle(item.style.replace(/\r|\n|\\s/g, ''));
+				const nodes = formatStyles(styleDatas);
+				// 先转原子类
+				[atomCss = [], otherNodes = []] = getGatherAtomCss(g_atom_gather, nodes);
+				const remainNodes = [];
+				otherNodes.forEach(item => {
+					const { prop, value } = item;
+					const atom = getAtomCss(g_atom_base, prop, value);
+					if (atom) {
+						atomCss.push(atom);
+						return
+					}
+					remainNodes.push(`${prop}:${value};`);
+				})
+				if (item.className && remainNodes && remainNodes.length) {
+					atomCss.push(item.className);
+					cssData[`.${item.className}`] = `.${item.className}{${remainNodes.join(' ')}}`;
+				} else if (remainNodes && remainNodes.length) {
+					dom.attr('style', remainNodes.join(''));
+				}
+			}
+			// 伪类
+			if (item.afterContent && item.className) {
+				const styleDatas = getCssNodesByStyle(item.afterContent.replace(/\r|\n|\\s/g, ''));
+				const nodes = formatStyles(styleDatas);
+				const [gatherAtomCss = [], gatherOtherNodes = []] = getGatherAtomCss(g_atom_gather, nodes, 1, 'after');
+				if (gatherAtomCss.length) {
+					atomCss = atomCss.concat(gatherAtomCss);
+				}
+				if (gatherOtherNodes.length) {
+					let afterStr = '';
+					gatherOtherNodes.forEach(gItem => {
+						afterStr = `${afterStr}${gItem.prop}:${gItem.value};`
+					})
+					cssData[`.${item.className}::after`] = `.${item.className}::after{${afterStr}}`;
+				}
+				if (!atomCss.includes(item.className)) {
+					atomCss.push(item.className);
+				}
+			}
+			if (atomCss.length) {
+				dom.attr('class', atomCss.join(' '));
+			}
 			parentDom.append(dom);
 			if (item.children && item.children.length) {
 				createDom(item.children, dom, cssData)
